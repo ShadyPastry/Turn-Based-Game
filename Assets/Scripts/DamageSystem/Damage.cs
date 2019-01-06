fde6c478adfb109f7a0e269b6a45c4870f101bc4
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-01-06 09:47:35 -0500

    Minor method refactoring + renaming in DamageSystem/Damage

--- Assets/Scripts/DamageSystem/Damage.cs
+++ Assets/Scripts/DamageSystem/Damage.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace DamageSystem {
-  [System.Flags] public enum DamageTypes {
+  [System.Flags]
+  public enum DamageTypes {
     Fire = 1 << 0,
     Ice = 1 << 1,
     Volt = 1 << 2,
@@ -11,7 +12,7 @@ namespace DamageSystem {
     Stab = 1 << 4,
     Bash = 1 << 5
   }
-    public static class TypeExtensions {
+  public static class DamageTypesExtensions {
     public static bool IsElemental(this DamageTypes damageType) {
       return damageType.HasFlag(DamageTypes.Fire | DamageTypes.Ice | DamageTypes.Volt);
     }
@@ -26,32 +27,43 @@ namespace DamageSystem {
     public delegate bool IsImmune(IDamageable victim);
     public static readonly IsImmune noImmunity = v => false;
 
-    //Damage within a box
-    private static void DamageBox(DamageTypes damageType, int baseDamage, Vector3 center, Vector3 halfExtents, IsImmune immunityCheck) {
-      Collider[] hits = Physics.OverlapBox(center, halfExtents + skinWidth);
-
-      foreach (var hit in hits) {
-        IDamageable victim = hit.transform.gameObject.GetComponent<IDamageable>();
-        if (victim != null && !immunityCheck(victim)) {
-          victim.DamageHp(damageType, baseDamage);
+    public static void DamageTargets(ICollection<IDamageable> targets, DamageTypes damageTypes, int baseDamage, IsImmune immunityCheck) {
+      foreach (IDamageable target in targets) {
+        if (target != null && !immunityCheck(target)) {
+          target.DamageHp(damageTypes, baseDamage);
         }
       }
     }
 
+    public static void DamageTargets(ICollection<Collider> targets, DamageTypes damageTypes, int baseDamage, IsImmune immunityCheck) {
+      foreach (Collider hit in targets) {
+        IDamageable target = hit.transform.gameObject.GetComponent<IDamageable>();
+        if (target != null && !immunityCheck(target)) {
+          target.DamageHp(damageTypes, baseDamage);
+        }
+      }
+    }
+
+    //Damage within a box
+    private static void DamageBox(DamageTypes damageTypes, int baseDamage, Vector3 center, Vector3 halfExtents, IsImmune immunityCheck) {
+      Collider[] hits = Physics.OverlapBox(center, halfExtents + skinWidth);
+      DamageTargets(hits, damageTypes, baseDamage, immunityCheck);
+    }
+
     //Damage whoever is touching tile (includes the sides of tile)
-    public static void DamageTouching(DamageTypes damageType, int baseDamage, GameTile tile, IsImmune immunityCheck) {
-      DamageBox(damageType, baseDamage, tile.transform.position, tile.transform.localScale / 2, immunityCheck);
+    public static void DamageTouching(DamageTypes damageTypes, int baseDamage, GameTile tile, IsImmune immunityCheck) {
+      DamageBox(damageTypes, baseDamage, tile.transform.position, tile.transform.localScale / 2, immunityCheck);
     }
 
     //On top of tile, envision a box with height verticalReach
     //Damage whoever is within that box
-    public static void DamageAbove(DamageTypes damageType, int baseDamage, GameTile tile, float verticalReach, IsImmune immunityCheck) {
+    public static void DamageAbove(DamageTypes damageTypes, int baseDamage, GameTile tile, float verticalReach, IsImmune immunityCheck) {
       Vector3 tileHalfExtents = tile.transform.localScale / 2;
 
       Vector3 center = tile.transform.position + new Vector3(0, tileHalfExtents.y + verticalReach / 2, 0);
       Vector3 halfExtents = tileHalfExtents;
       halfExtents.y = verticalReach / 2;
-      DamageBox(damageType, baseDamage, center, halfExtents, immunityCheck);
+      DamageBox(damageTypes, baseDamage, center, halfExtents, immunityCheck);
     }
   }
 }
