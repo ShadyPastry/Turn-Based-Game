ba106e59106c7aee175e254bbaf5675b6d848da5
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-01-07 16:10:52 -0500

    Rename ISpellbookModifier to IRuneModifier

---
+++ Assets/Scripts/SpellSystem/IRuneModifier.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpellSystem {
+  //Modifies the energy adjustments made to an ISpellbook when ISpellbook.AddRune() is called
+  public interface IRuneModifer {
+    //An ADDITIVE modifier that can add or remove additional RuneEnergy based on rune
+    IEnumerable<RuneEnergy> RuneEnergyBonuses(Rune rune);
+  }
+}
