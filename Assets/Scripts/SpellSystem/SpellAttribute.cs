3a367de6a2686df0c7739a688225c508377599f7
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-01-07 16:11:26 -0500

    Remove unused using statements from SpellAttribute

--- Assets/Scripts/SpellSystem/SpellAttribute.cs
+++ Assets/Scripts/SpellSystem/SpellAttribute.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using static SpellSystem.SpellAttribute;
+﻿using static SpellAttribute;
 
-namespace SpellSystem {
-  public enum SpellAttribute {
-    Fire, Ice, Volt, //Damage types
-    Power, Stability, //Max and min damage
-    Efficiency //TP cost
-  }
+public enum SpellAttribute {
+  Fire, Ice, Volt, //Damage types
+  Power, Stability, //Max and min damage
+  Efficiency //TP cost
+}
 
-  public static class SpellAttributeExtensions {
-    public static bool IsElemental(this SpellAttribute attr) {
-      return attr == Fire || attr == Ice || attr == Volt;
-    }
+public static class SpellAttributeExtensions {
+  public static bool IsElemental(this SpellAttribute attr) {
+    return attr == Fire || attr == Ice || attr == Volt;
   }
 }
