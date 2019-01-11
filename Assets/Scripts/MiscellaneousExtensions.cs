61e92b88550270c4bc74a3232be7d1efb93d7850
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-01-11 11:29:29 -0500

    Add HashSet.ForEach extension
    Now using a single file for miscellaneous extensions to EXISTING libraries

--- Assets/Scripts/MiscellaneousExtensions.cs
+++ Assets/Scripts/MiscellaneousExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public static class ColorExtensions {
   public static Color Randomize(this Color color, float minusVariance, float plusVariance) {
@@ -6,5 +7,13 @@ public static class ColorExtensions {
     float g = Mathf.Clamp01(Random.Range(color.g * minusVariance, color.g * plusVariance));
     float b = Mathf.Clamp01(Random.Range(color.b * minusVariance, color.b * plusVariance));
     return new Color(r, g, b);
+  }
+}
+
+public static class HashSetExtensions {
+  public static void ForEach<T>(this HashSet<T> hashSet, System.Action<T> action) {
+    foreach (T item in hashSet) {
+      action(item);
+    }
   }
 }
